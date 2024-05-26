e1055bf49ffcb1276b7804166faff8a701c6bb69
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2024-05-26 17:21:21 +0530

    Implement dummy ping command

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+export JAVA_HOME="/usr/lib/jvm/java-1.21.0-openjdk-amd64/"
+
 mvn -B --quiet package -Ddir=/tmp/codecrafters-redis-target
 exec java -jar /tmp/codecrafters-redis-target/java_redis.jar "$@"
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,50 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    private static final String PING_COMMAND = "PING";
+    private static final String PING_OUTPUT = "+PONG\\r\\n";
 
-    //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+
         int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
+        String pingCommand = "PING";
+        try (
+                ServerSocket serverSocket = new ServerSocket(port);
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        ) {
+            String inputLine, outputLine;
+
+            out.printf("+PONG\r\n");
+
+
+//            while ((inputLine = bufferedReader.readLine()) != null) {
+//                outputLine = processCommand(inputLine.toUpperCase());
+//
+//
+////                out.print(outputLine);
+////
+////                if (outputLine != null) {
+////                    break;
+////                }
+//            }
+
+        } catch (Exception e) {
             System.out.println("IOException: " + e.getMessage());
-          }
         }
-  }
+    }
+
+    private static String processCommand(String command) {
+        return switch (command) {
+            case PING_COMMAND -> PING_OUTPUT;
+
+            default -> null;
+        };
+    }
 }
