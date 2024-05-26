990dc42f9884b3a07961f042cbb7f13c3f0a6e26
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2024-05-26 17:22:32 +0530

    Implement dummy mutiple ping command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,40 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     private static final String PING_COMMAND = "PING";
-    private static final String PING_OUTPUT = "+PONG\\r\\n";
+    private static final String PING_OUTPUT = "+PONG\r\n";
 
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
         int port = 6379;
-        String pingCommand = "PING";
-        try (
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket clientSocket = serverSocket.accept();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        ) {
-            String inputLine, outputLine;
 
-            out.printf("+PONG\r\n");
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            serverSocket.setReuseAddress(true);
 
+            try (
+                    Socket clientSocket = serverSocket.accept();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    OutputStream outputStream = clientSocket.getOutputStream();
+            ) {
+                String inputLine, outputLine;
 
-//            while ((inputLine = bufferedReader.readLine()) != null) {
-//                outputLine = processCommand(inputLine.toUpperCase());
-//
-//
-////                out.print(outputLine);
-////
-////                if (outputLine != null) {
-////                    break;
-////                }
-//            }
+                while ((inputLine = bufferedReader.readLine()) != null) {
+                    outputLine = processCommand(inputLine.toUpperCase());
+                    System.out.println("input: " + inputLine + " | out: " + outputLine);
+
+                    if (outputLine != null) {
+                        outputStream.write(outputLine.getBytes(StandardCharsets.UTF_8));
+                    }
+                }
+            }
 
         } catch (Exception e) {
             System.out.println("IOException: " + e.getMessage());
@@ -43,7 +44,7 @@ public class Main {
     private static String processCommand(String command) {
         return switch (command) {
             case PING_COMMAND -> PING_OUTPUT;
-
+            case "COMMAND" -> "+OK\r\n";
             default -> null;
         };
     }
